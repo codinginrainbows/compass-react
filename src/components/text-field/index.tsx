f9161d0fb06e615d2913778d940a64d26a4f8ed1
gabriel garcia <gabrielgarcia.contactme@gmail.com>
2023-04-26 10:55:50 -0300

    refactor: remove reactHookForm from SignIn

--- src/components/text-field/index.tsx
+++ src/components/text-field/index.tsx
@@ -5,12 +5,20 @@ interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
     content: string;
     type?: string;
     icon?: any;
+    textValue?: string;
+    changeEvent?: (e: any) => void;
 }
 
 function TextField(props: TextFieldProps) {
     return (
         <>
-            <S.Input {...props} placeholder={props.content} autoComplete='off' />
+            <S.Input 
+                {...props} 
+                placeholder={props.content} 
+                autoComplete='off'
+                value={props.textValue} 
+                onChange={() => props.changeEvent} 
+            />
             <S.Wrapper>
                 <S.Icon src={props.icon} />
             </S.Wrapper>
--- src/templates/sign-in/index.tsx
+++ src/templates/sign-in/index.tsx
@@ -7,9 +7,6 @@ import { TextLink } from "../../components/text-link"
 import userIcon from "../../assets/icons/user-icon.svg"
 import lockIcon from "../../assets/icons/lock-icon.svg"
 
-import { Controller, useForm } from "react-hook-form"
-
-import { SignInValidationSchema, defaultValues } from "./schema"
 import { ErrorMessage } from "../../components/error-message"
 import { useState } from "react"
 import { ISignIn } from "../../models/sign-in"
@@ -20,16 +17,21 @@ import { useAccount } from "../../hooks/account"
 
 function SignInTemplate() {
     const [checkCredentials, setCheckCredentials] = useState(true)
+    const [hasError, setHasError] = useState(false)
+    const [formValues, setFormValues] = useState({ user: '', password: '' })
+
+    const handleChangeValues = (e: any) => {
+        const textFieldName = e.target.name
+        const textFieldValue = e.target.value
+
+        setFormValues((currentValues => {
+            return {
+                ...currentValues, [textFieldName]: textFieldValue
+            }
+        }))
+    }
 
     const { credentials } = useAccount()
-
-    const {
-        control,
-        handleSubmit,
-    } = useForm({
-        resolver: SignInValidationSchema,
-        defaultValues: defaultValues,
-    })
 
     const handleSubmitForm = ({ user: userTyped, password: passwordTyped }: ISignIn) => {
         if(credentials.user === userTyped && credentials.password === passwordTyped) {
@@ -44,60 +46,40 @@ function SignInTemplate() {
     return (
         <S.Wrapper>
             <S.Content>
-                <S.Form onSubmit={handleSubmit(handleSubmitForm)}>
+                <S.Form onSubmit={() => handleSubmitForm}>
                     <FormHeader title="Olá," subTitle="Para continuar navegando de forma segura, efetue o login" />
                     
                     <FormTitle content="Login" />
                     
-                    <Controller
-                        name="user"
-                        control={control}
-                        render={({
-                            field: { ref, ...fieldProps },
-                            fieldState: { error },
-                        }) => (
-                            <>
-                                <TextField 
-                                    {...fieldProps}
-                                    content="Usuário" 
-                                    type="text" 
-                                    icon={userIcon} 
-                                    className={error && 'input-invalid'}
-                                />
-                                {error && (
-                                    <>
-                                        <ErrorMessage text={error.message}/>
-                                        {setCheckCredentials(true)}
-                                    </>
-                                )}
-                            </>
-                        )}
+                    <TextField 
+                        content="Usuário" 
+                        type="text" 
+                        icon={userIcon}
+                        textValue={formValues.user}
+                        changeEvent={handleChangeValues}
+                        // className={hasError && 'input-invalid'}
                     />
-                    
-                    <Controller
-                        name="password"
-                        control={control}
-                        render={({
-                        field: { ref, ...fieldProps },
-                        fieldState: { error },
-                    }) => (
-                            <>
-                                <TextField 
-                                    {...fieldProps}
-                                    content="Senha" 
-                                    type="password" 
-                                    icon={lockIcon}
-                                    className={error && 'input-invalid'}
-                                />
-                                {error && (
-                                    <>
-                                        <ErrorMessage text={error.message}/>
-                                        {setCheckCredentials(true)}
-                                    </>
-                                )}
-                            </>
-                        )}
-                        />
+                    {hasError && (
+                        <>
+                            <ErrorMessage text='Preencha um nome de usuário'/>
+                            {setCheckCredentials(true)}
+                        </>
+                    )}
+                          
+                    <TextField 
+                        content="Senha" 
+                        type="password" 
+                        icon={lockIcon}
+                        textValue={formValues.password}
+                        changeEvent={handleChangeValues}
+                        // className={hasError && 'input-invalid'}
+                    />
+                    {hasError && (
+                        <>
+                            <ErrorMessage text='Preencha uma senha'/>
+                            {setCheckCredentials(true)}
+                        </>
+                    )}
 
                     {!checkCredentials && <ErrorMessage text='Usuário e/ou Senha inválidos. Por favor, tente novamente!'/>}
                     
