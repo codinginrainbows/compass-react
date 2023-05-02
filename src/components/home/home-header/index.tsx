b194f68e8a909c816f64042139e049dbb2c00e3c
Gabriel Garcia <gabrielmgarcia96@gmail.com>
2023-05-01 21:55:19 -0300

    feat: header get name from storage

--- src/components/home/home-header/index.tsx
+++ src/components/home/home-header/index.tsx
@@ -1,12 +1,15 @@
 import * as S from './styles'
 import HomeIcon from '../../../assets/icons/home/home.svg'
 import { ProfilePicture } from '../../profile-picture'
+import { useAccount } from '../../../hooks/useAccount'
 
 interface IHeader {
     gridPosition: string
 }
 
 function Header({ gridPosition }: IHeader) {
+    const { name: nameFromStorage } = useAccount()
+
     return (
         <S.Wrapper className={gridPosition}>
             <S.HomeInfoContainer>
@@ -14,7 +17,7 @@ function Header({ gridPosition }: IHeader) {
                 <S.HomeText>Home</S.HomeText>
             </S.HomeInfoContainer>
             <S.HomeInfoContainer>
-                <S.HomeText>Gabriel Garcia</S.HomeText>
+                <S.HomeText>{nameFromStorage}</S.HomeText>
                 <ProfilePicture imageAdress='https://picsum.photos/45' />
             </S.HomeInfoContainer>
         </S.Wrapper>
